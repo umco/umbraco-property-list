f1eb492d162b82d11481cdae206e91d7cda41d5f
leekelleher <leekelleher@gmail.com>
2018-01-11 17:28:00 +0000

    Bumped version to v1.0.0-beta
    Added placeholder logo (black spot)
    Switched AppVeyor to use VS2017 image.

--- src/Our.Umbraco.PropertyList/Properties/VersionInfo.cs
+++ src/Our.Umbraco.PropertyList/Properties/VersionInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("0.1.*")]
-[assembly: AssemblyInformationalVersion("0.1.0-develop")]
+[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyInformationalVersion("1.0.0-develop")]
 
 
