a64d95e0ccc31a06449c2f2923a7e5583fef0ff0
leekelleher <leekelleher@gmail.com>
2018-07-16 17:45:25 +0100

    Implemented the `IPropertyValidator` class
    This will make calls to the nested property validators

---
+++ src/Our.Umbraco.PropertyList/PropertyListConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace Our.Umbraco.PropertyList
+{
+    internal static class PropertyListConstants
+    {
+        public static class PreValueKeys
+        {
+            public const string DataType = "dataType";
+
+            public const string MinItems = "minItems";
+
+            public const string MaxItems = "maxItems";
+
+            public const string HideLabel = "hideLabel";
+        }
+    }
+}
